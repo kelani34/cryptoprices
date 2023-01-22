a6c417fa6820dace3292ec0ad7d48f4ce10f54d5
kelani34 <kelanitaiwo34@gmail.com>
2023-01-22 21:54:24 +0100

    initialize api request with axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+
+export type Coins = {
+  ath: number;
+  atl: number;
+  current_price: number;
+  id: string;
+  name: string;
+  symbol: string;
+  high_24h: number;
+  low_24h: number;
+};
 function App() {
+  const [coins, setCoins] = useState<Coins[] | null>();
   useEffect(() => {
     const url =
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
-      
-  });
+
+    axios.get(url).then((res) => {
+      setCoins(res.data);
+    });
+  }, []);
   return (
     <div className="App">
-      <div>hello</div>
+      {coins
+        ? coins.map((coin) => {
+            return (
+              <p>{`${coin.name} | ${coin.symbol} | ${coin.current_price} `}</p>
+            );
+          })
+        : null}
     </div>
   );
 }
