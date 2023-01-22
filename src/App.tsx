9809edaf228d0bd1f460026cb92f4e3fde58bb46
kelani34 <kelanitaiwo34@gmail.com>
2023-01-22 21:33:43 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect } from "react";
+import axios from "axios";
 function App() {
+  useEffect(() => {});
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>hello</div>
     </div>
   );
 }
