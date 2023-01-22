36b83f5bbdbfc3156bcd919943835dde71fc1a74
kelani34 <kelanitaiwo34@gmail.com>
2023-01-22 21:34:42 +0100

    add: coingeko api link

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 function App() {
-  useEffect(() => {});
-  const url =
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+  useEffect(() => {
+    const url =
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+      
+  });
   return (
     <div className="App">
       <div>hello</div>
