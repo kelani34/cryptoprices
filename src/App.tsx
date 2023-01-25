8fb94f12d5f06013f9f53dae8bb2db15e9d70052
kelani34 <kelanitaiwo34@gmail.com>
2023-01-25 14:33:27 +0100

    compare and make calculations with coin price

--- src/App.tsx
+++ src/App.tsx
@@ -27,10 +27,10 @@ ChartJS.register(
 
 function App() {
   const [coins, setCoins] = useState<Coins[] | null>();
-  const [selected, setSelected] = useState<Coins | null>();
-  const [data, setData] = useState<ChartData<"line">>();
+  const [selected, setSelected] = useState<Coins[]>([]);
+  // const [data, setData] = useState<ChartData<"line">>();
   const [range, setRange] = useState<number>(30);
-  const [options, setOptions] = useState<ChartOptions<"line">>({});
+  // const [options, setOptions] = useState<ChartOptions<"line">>({});
   useEffect(() => {
     const url =
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
@@ -40,54 +40,54 @@ function App() {
     });
   }, []);
 
-  useEffect(() => {
-    if (!selected) return;
-    axios
-      .get(
-        `https://api.coingecko.com/api/v3/coins/${
-          selected?.id
-        }/market_chart?vs_currency=usd&days=${range}&interval=${
-          range === 1 ? "hourly" : "daily"
-        }
-            `
-      )
-      .then((res) => {
-        setData({
-          labels: res.data.prices.map((x: number[]) =>
-            moment.unix(x[0] / 1000).format(range === 1 ? "HH:MM" : "MM-DD")
-          ),
-          datasets: [
-            {
-              label: "Dataset 1",
-              data: res.data.prices.map((x: number[]) => x[1]),
-              borderColor: "rgb(255, 99, 132)",
-              backgroundColor: "rgba(255, 99, 132, 0.5)",
-            },
-          ],
-        });
-      });
-    setOptions({
-      responsive: true,
-      plugins: {
-        legend: {
-          display: false,
-        },
-        title: {
-          display: true,
-          text: `Last price of ${selected?.id} in the last ${
-            range === 1 ? " 24 Hours" : `${range} Days`
-          }`,
-        },
-      },
-    });
-  }, [selected, range]);
+  // useEffect(() => {
+  //   if (!selected) return;
+  //   axios
+  //     .get(
+  //       `https://api.coingecko.com/api/v3/coins/${
+  //         selected?.id
+  //       }/market_chart?vs_currency=usd&days=${range}&interval=${
+  //         range === 1 ? "hourly" : "daily"
+  //       }
+  //           `
+  //     )
+  //     .then((res) => {
+  //       setData({
+  //         labels: res.data.prices.map((x: number[]) =>
+  //           moment.unix(x[0] / 1000).format(range === 1 ? "HH:MM" : "MM-DD")
+  //         ),
+  //         datasets: [
+  //           {
+  //             label: "Dataset 1",
+  //             data: res.data.prices.map((x: number[]) => x[1]),
+  //             borderColor: "rgb(255, 99, 132)",
+  //             backgroundColor: "rgba(255, 99, 132, 0.5)",
+  //           },
+  //         ],
+  //       });
+  //     });
+  //   setOptions({
+  //     responsive: true,
+  //     plugins: {
+  //       legend: {
+  //         display: false,
+  //       },
+  //       title: {
+  //         display: true,
+  //         text: `Last price of ${selected?.id} in the last ${
+  //           range === 1 ? " 24 Hours" : `${range} Days`
+  //         }`,
+  //       },
+  //     },
+  //   });
+  // }, [selected, range]);
   return (
     <>
       <div className="App">
         <select
           onChange={(e) => {
-            const c = coins?.find((x) => x.id === e.target.value);
-            setSelected(c);
+            const c = coins?.find((x) => x.id === e.target.value) as Coins;
+            setSelected([...selected, c]);
           }}
           defaultValue="default"
         >
@@ -102,7 +102,7 @@ function App() {
               })
             : null}
         </select>
-        <select
+        {/* <select
           onChange={(e) => {
             setRange(parseInt(e.target.value));
           }}
@@ -111,14 +111,17 @@ function App() {
           <option value={30}>30 days</option>
           <option value={7}>7 days </option>
           <option value={1}> 1 day</option>
-        </select>
+        </select> */}
       </div>
-      {selected ? <CoinsSummary coin={selected} /> : null}
-      {data ? (
+      {selected.map((s) => (
+        <CoinsSummary coin={s} />
+      ))}
+      {/* {selected ? <CoinsSummary coin={selected} /> : null} */}
+      {/* {data ? (
         <div>
           <Line data={data} options={options} />
         </div>
-      ) : null}
+      ) : null} */}
     </>
   );
 }
--- src/components/CoinsSummary.tsx
+++ src/components/CoinsSummary.tsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AppProp } from "../types/Types";
 
 const CoinsSummary = ({ coin }: AppProp): JSX.Element => {
-  return <p>{`${coin.name} | ${coin.symbol} | ${coin.current_price} `}</p>;
+  const [amount, setAmount] = useState<number>(0);
+
+  useEffect(() => {
+    console.log(coin.name + " set amount", amount);
+  });
+  return (
+    <div>
+      <span>{`${coin.name} | ${coin.symbol} | ${coin.current_price} `}</span>
+      <input
+        style={{ margin: "10px" }}
+        type="number"
+        value={amount}
+        onChange={(e) => setAmount(parseInt(e.target.value))}
+      />
+      <p>
+        {(coin.current_price * amount).toLocaleString(undefined, {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2,
+        })}
+      </p>
+    </div>
+  );
 };
 
 export default CoinsSummary;
